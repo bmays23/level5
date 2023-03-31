bebd6bcca7c4830ee71278a09d89092aa11d9c0c
Brittany Mays <brttnymays@gmail.com>
2023-03-31 16:11:58 -0400

    bounty hunter two final

--- bounty-hunter-one/bountyRouter.js
+++ bounty-hunter-one/bountyRouter.js
@@ -11,14 +11,27 @@ bountyRouter.get("/", (req,res) => {
 })
 
 
-bountyRouter.post("/bounty", (req,res) => {
+bountyRouter.post("/", (req,res) => {
     const newBounty = req.body
     newBounty._id = uuidv4()
     bounty.push(newBounty)
     res.send(`Added ${newBounty.firstName} to the database!`)
 })
 
+bountyRouter.delete("/:bountyID", (req,res) => {
+    const bountyID = req.params.bountyID
+    const bountyIndex = bounty.findIndex(bounty => bounty._id === bountyID)
+    bounty.splice(bountyIndex,1)
+    res.send("Sucessfully deleted movie")
+    
+})
 
+bountyRouter.put("/:bountyID", (req,res) => {
+    const bountyID = req.params.bountyID
+    const bountyIndex = bounty.findIndex(bounty => bounty._id === bountyID)
+    const updatedBounty = Object.assign(bounty[bountyIndex], req.body)
+    res.send(updatedBounty)
+})
 
 
 
